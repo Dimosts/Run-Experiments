948405cb6f67b437518547c1a89002be8c6cde20
Dimos Tsouros <dimosts988@gmail.com>
2025-03-04 19:39:51 +0100

    (not) saving results

--- runexp/__init__.py
+++ runexp/__init__.py
@@ -6,11 +6,11 @@ from multiprocessing import cpu_count
 def default_parser():
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument("config", type=str, help="Configuration file, should be json-formatted")
     #parser.add_argument("func", type=str, help="Main function of the experiment")
     #parser.add_argument("runner", type=str, help="runexp Runner subclass to call")
     parser.add_argument("--output", type=str, default="results_dir", help="Directory to output results of experiments")
+    parser.add_argument("-s", "--save_results", action="store_true", help="Whether to save results of experiments")
     parser.add_argument("-u", "--unravel", action="store_true", help="Whether to unravel config file to run experiments in a batch (will unravel lists in configuration file to separate configs)")
     parser.add_argument("--parallel", action="store_true", help="Wheter to run experiments in paralell, only useful if `--unravel` is True")
     parser.add_argument("--num-workers", action="store", type=int, default=cpu_count()-1, help=f"Number of threads to use for parallelization (=nb of experiments running in parallel, default={cpu_count()-1})")
